03788d977a50790f38e706c5e6a40fafeda6aa26
amandzy <amandzy@pratt.edu>
2023-11-14 16:17:58 -0500

    Playing with NPCDetection

--- Assets/Scripts/NPCDetect.cs
+++ Assets/Scripts/NPCDetect.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NPCDetect : MonoBehaviour
 {
+    bool player_detection = false;
 
-    void OnTriggerEnter(Collider other)
+    void Update()
     {
-        Debug.Log("Dialogue");
+        if (player_detection == true)
+        {
+            if (Keyboard.current.fKey.isPressed)
+            {
+                Debug.Log("Dialogue start");
+            }
+        }
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("DialogueInteractPopUp");
+        if (other.name == "Player")
+        {
+            player_detection = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        player_detection = false;
+    }
 
 }
