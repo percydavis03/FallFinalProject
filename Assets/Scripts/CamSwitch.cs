92553cdf06f12a3811f759dc13f71f726337d081
DESKTOP-1SN7JO2\Leonar Montano <Lmontano@pratt.edu>
2023-12-14 11:12:19 -0500

    I finshed having the stone not active until you touch the stone

--- Assets/Scripts/CamSwitch.cs
+++ Assets/Scripts/CamSwitch.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class CamSwitch : MonoBehaviour
 {
@@ -8,11 +9,25 @@ public class CamSwitch : MonoBehaviour
     public GameObject Hagstonecam;
     public GameObject Hagstone;
     private bool stoneisActive;
+    private bool itemActive;
+    private List<Item> Itemsholder = new List<Item>();
     void Update()
     {
-        print(stoneisActive);
-        if (Input.GetKeyDown(KeyCode.C))
+        //Itemsholder = InventoryManager.Instance.Items;
+        /*foreach(Item item in Itemsholder)
         {
+            if (item.name == "Stone")
+            {
+                
+                itemActive = true;
+                return;
+            }
+            
+        }*/
+        print(itemActive);
+        if (Input.GetKeyDown(KeyCode.C) && InventoryManager.Instance.itemActive == true)
+        {
+            print("Active");
             if (stoneisActive == false)
             {
                 maincam.SetActive(false);
